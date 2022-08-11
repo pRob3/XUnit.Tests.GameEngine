e582d4c29d52b8efdbcc39eb9e38050cce4540c7
dennis.asplind <dennis.asplind@netonnet.com>
2022-08-11 14:54:12 +0200

    bool and string tests

--- GameEngineFF.Test/PlayerCharacterShould.cs
+++ GameEngineFF.Test/PlayerCharacterShould.cs
@@ -12,5 +12,71 @@ namespace GameEngine.Tests
 
             Assert.True(sut.IsNoob);
         }
+
+        [Fact]
+        public void CalculateFullName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "John";
+            sut.LastName = "Smith";
+
+            Assert.Equal("John Smith", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFullNameStartingWithFirstName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "John";
+            sut.LastName = "Smith";
+
+            Assert.StartsWith("John", sut.FullName);
+        }
+
+        [Fact]
+        public void HaveFullNameEndingWithLastName()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "John";
+            sut.LastName = "Smith";
+
+            Assert.EndsWith("Smith", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullName_IgnoreCaseAssertExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "JOHN";
+            sut.LastName = "SMITH";
+
+            Assert.EndsWith("Smith", sut.FullName, StringComparison.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public void CalculateFullName_SubstringAssertExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "John";
+            sut.LastName = "Smith";
+
+            Assert.Contains("hn Sm", sut.FullName);
+        }
+
+        [Fact]
+        public void CalculateFullNameWithTitleCase()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.FirstName = "John";
+            sut.LastName = "Smith";
+
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
+        }
     }
 }
