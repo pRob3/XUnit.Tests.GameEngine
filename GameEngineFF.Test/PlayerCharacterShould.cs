18f3fe06eb2903f528f4bc32e54235bff3cf7960
dennis.asplind <dennis.asplind@netonnet.com>
2022-08-26 15:05:05 +0200

    Player health tests

--- GameEngineFF.Test/PlayerCharacterShould.cs
+++ GameEngineFF.Test/PlayerCharacterShould.cs
@@ -164,4 +164,34 @@ public class PlayerCharacterShould : IDisposable
         Assert.PropertyChanged(_sut, "Health", () => _sut.TakeDamage(10));
     }
 
+    [Fact]
+    public void TakeZeroDamage()
+    {
+        _sut.TakeDamage(0);
+
+        Assert.Equal(100, _sut.Health);
+    }
+
+    [Fact]
+    public void TakeSmallDamage(){
+        _sut.TakeDamage(1);
+
+        Assert.Equal(99, _sut.Health);
+    }
+
+    [Fact]
+    public void TakeMediumDamage()
+    {
+        _sut.TakeDamage(50);
+
+        Assert.Equal(50, _sut.Health);
+    }
+
+    [Fact]
+    public void HaveMinimum1Health()
+    {
+        _sut.TakeDamage(101);
+
+        Assert.Equal(1, _sut.Health);
+    }
 }
